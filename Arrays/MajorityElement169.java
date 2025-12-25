b601ffa6b9b25a97d00bb9c79bf16723a255830c
Bharath-chary02 <kadiyalabharath98@gmail.com>
2025-12-25 16:39:56 +0530

    Add brute force solution for Majority Element (O(n^2))

---
+++ Arrays/MajorityElement169.java
@@ -0,0 +1,42 @@
+// import java.util.Arrays;
+
+public class MajorityElement169 {
+    public static void main(String[] args) {
+        int nums[] = {3, 2, 3};
+        Solution sol = new Solution();
+
+        System.out.println(sol.majorityElement(nums));
+    }
+}
+
+// Example 1:
+
+// Input: nums = [3,2,3]
+// Output: 3
+
+class Solution {
+    public int majorityElement(int[] nums) {
+        int n = nums.length;
+
+        // ---------------------------------------------
+        // Approach 1: Brute Force
+        // Time: O(n * n)
+        // Space: O(1)
+
+        // int count = 0;
+        // for(int i = 0; i < n; i++){
+        //     for(int j = 0; j < n; j++){
+        //         if(nums[i] == nums[j]){
+        //             count++;
+        //         }
+        //     }
+        //     if(count > n / 2){
+        //         return nums[i];
+        //     }
+        //     count = 0;
+        // }
+        // return nums[i];
+
+        
+    }
+}
