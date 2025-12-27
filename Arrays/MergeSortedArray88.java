c1adf0000b74587cbf5fcbdb944d2e7770b4f351
Bharath-chary02 <kadiyalabharath98@gmail.com>
2025-12-27 17:24:25 +0530

    Implement merge using two pointers with auxiliary array

--- Arrays/MergeSortedArray88.java
+++ Arrays/MergeSortedArray88.java
@@ -45,6 +45,35 @@ class Solution {
         }
         */
 
+        // ---------------------------------------------
+        // Approach 2: Two Pointers + Extra Array
+        // Time: O(m + n)
+        // Space: O(m + n)
+
+        /*
+        int[] res = new int[m + n];
+        int i = 0, j = 0, k = 0;
+
+        while(i < m && j < n) {
+            if(nums1[i] <= nums2[j]) {
+                res[k++] = nums1[i++];
+            } else {
+                res[k++] = nums2[j++];
+            }
+        }
+
+        while(i < m) {
+            res[k++] = nums1[i++];
+        }
+        while(j < n) {
+            res[k++] = nums2[j++];
+        }
+
+        for(int x = 0; x < m + n; x++) {
+            nums1[x] = res[x];
+        }
+        */
+
         
     }
 }
