7d810c2ec4d10da7d7bdce45a6bd2956e2836952
Bharath-chary02 <kadiyalabharath98@gmail.com>
2025-12-30 17:07:55 +0530

    Add prefix sum approach for Maximum Subarray

---
+++ Arrays/MaximumSubarray53.java
@@ -0,0 +1,44 @@
+public class MaximumSubarray53 {
+    public static void main(String[] args) {
+        int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+
+        Solution sol = new Solution();
+
+        System.out.println(sol.maxSubArray(nums));
+    }
+}
+
+// Example 1:
+
+// Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+// Output: 6
+// Explanation: The subarray [4,-1,2,1] has the largest sum 6.
+
+class Solution {
+    public int maxSubArray(int[] nums) {
+
+        // Brute Force (O(n^2)) – Not accepted due to TLE
+
+        // int[] prefix = new int[nums.length];
+        // int currSum = 0;
+        // int maxSum = Integer.MIN_VALUE;
+
+        // prefix[0] = nums[0];
+        // for(int i = 1; i < nums.length; i++) {
+        //     prefix[i] = prefix[i - 1] + nums[i];
+        // }
+
+        // for(int i = 0; i < nums.length; i++) {
+        //     for(int j = i; j < nums.length; j++) {
+        //         currSum = i == 0 ? prefix[j] : prefix[j] - prefix[i - 1];
+        //         if(currSum > maxSum) {
+        //             maxSum = currSum;
+        //         }
+        //     }
+        // }
+
+        // return maxSum;
+
+        
+    }
+}
