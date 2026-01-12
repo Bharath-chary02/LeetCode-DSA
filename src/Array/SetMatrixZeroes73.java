aa95f97cda1728d879d43928cce132fc86c14172
Bharath-chary02 <kadiyalabharath98@gmail.com>
2026-01-12 20:10:29 +0530

    Implement improved solution using row and column marker arrays

--- src/Array/SetMatrixZeroes73.java
+++ src/Array/SetMatrixZeroes73.java
@@ -62,6 +62,41 @@ class Solution {
         }
         */
 
-        
+        // ---------------------------------------------
+        // Approach 2: Better (Using row & column arrays)
+        // Time: O(n * m)
+        // Space: O(n + m)
+        /*
+        int n = matrix.length;
+        int m = matrix[0].length;
+
+        int[] rows = new int[n];
+        int[] cols = new int[m];
+
+        for (int i = 0; i < n; i++) {
+            rows[i] = -1;
+        }
+        for (int j = 0; j < m; j++) {
+            cols[j] = -1;
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (matrix[i][j] == 0) {
+                    rows[i] = 0;
+                    cols[j] = 0;
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (rows[i] == 0 || cols[j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        */
+
     }
 }
