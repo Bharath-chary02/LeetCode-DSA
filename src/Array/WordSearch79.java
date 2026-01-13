9106a5fed23a45ab1d7cfc6c7bb12776d63822a7
Bharath-chary02 <kadiyalabharath98@gmail.com>
2026-01-13 18:02:07 +0530

    Optimize Word Search by marking board cells instead of visited array

--- src/Array/WordSearch79.java
+++ src/Array/WordSearch79.java
@@ -73,4 +73,48 @@ class Solution {
     //     return false;
     // }
 
+    // -------------------------------------------------
+    // Approach 2: Marking directly on board (Optimized Space)
+    // Time: O(n*m*4^L)
+    // Space: O(L) recursion stack
+
+    public static boolean helper(int row, int col, int n, int m, char[][] board, int index, int size, String word) {
+        if(index == size) {
+            return true;
+        }
+        char ch = board[row][col];
+        board[row][col] = '$';
+
+        for(int i = 0; i < 4; i++) {
+            int ur = row + r[i];
+            int uc = col + c[i];
+            if(ur >= 0 && ur < n && uc >= 0 && uc < m) {
+                if(board[ur][uc] == word.charAt(index)) {
+                    if(helper(ur, uc, n, m, board, index + 1, size, word)) {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        board[row][col] = ch;
+        return false;
+    }
+
+    public boolean exist(char[][] board, String word) {
+        int n = board.length;
+        int m = board[0].length;
+        int size = word.length();
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(board[i][j] == word.charAt(0)) {
+                    if(helper(i, j, n, m, board, 1, size, word)) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
 }
