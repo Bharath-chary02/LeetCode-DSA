5fce06f301d780b5a03edebd9ff24f8775e735bb
Bharath-chary02 <kadiyalabharath98@gmail.com>
2026-01-14 22:24:55 +0530

    Optimize productExceptSelf using prefix and suffix products (O(n), no division)

--- src/Array/ProductofArrayExceptSelf238.java
+++ src/Array/ProductofArrayExceptSelf238.java
@@ -78,6 +78,24 @@ class Solution {
         
         // return ans;
 
-    
+    // ---------------------------------------------
+    //  Approach 3: Prefix + Suffix (No division Best)
+    // Time: O(n)
+    // Space: O(1) extra (excluding output array)
+
+        int n = nums.length;
+        int[] ans = new int[n];
+        Arrays.fill(ans, 1);
+        for(int i = 1; i < n; i++) {
+            ans[i] = ans[i - 1] * nums[i - 1];
+        }
+
+        int suffix = nums[n - 1];
+        for(int i = n - 2; i >= 0; i--) {
+            ans[i] = ans[i] * suffix;
+            suffix *= nums[i];
+        }
+
+        return ans;
     }
 }
