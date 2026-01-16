ff82ea61fc39c3022e3987e1822baf9578cc60e3
Bharath-chary02 <kadiyalabharath98@gmail.com>
2026-01-16 20:35:18 +0530

    Optimize duplicate detection using Floyd cycle method

--- src/Array/FindtheDuplicateNumber287.java
+++ src/Array/FindtheDuplicateNumber287.java
@@ -56,6 +56,24 @@ class Solution {
 
         // return res;
 
-        
+        // ---------------------------------------------
+        // Approach 3: Floyd’s Tortoise & Hare (Cycle Detection)
+        // Time: O(n)
+        // Space: O(1)
+
+        int slow = nums[0];
+        int fast = nums[0];
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while(slow != fast);
+
+        slow = nums[0];
+        while(slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+
+        return fast;
     }
 }
