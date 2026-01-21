b3c8efa6205c885c9c50556723bf92a623800c9b
Bharath-chary02 <kadiyalabharath98@gmail.com>
2026-01-21 18:45:45 +0530

    Implement prefix/suffix max arrays method (O(n) time, O(n) space)

--- src/Array/TrappingRainWater42.java
+++ src/Array/TrappingRainWater42.java
@@ -41,6 +41,32 @@ class Solution {
 
         // return ans;
 
+        // -------------------------------------------------------
+        // Approach 2: Prefix + Suffix Arrays
+        // Time: O(n)
+        // Space: O(n)
+
+        // int n = height.length;
+        // int ans = 0;
+
+        // int[] leftMax = new int[n];
+        // leftMax[0] = height[0];
+        // for(int i = 1; i < n; i++) {
+        //     leftMax[i] = Math.max(height[i], leftMax[i - 1]);
+        // }
+
+        // int[] rightMax = new int[n];
+        // rightMax[n - 1] = height[n - 1];
+        // for(int i = n - 2; i >= 0; i--) {
+        //     rightMax[i] = Math.max(height[i], rightMax[i + 1]);
+        // }
+
+        // for(int i = 1; i < n - 1; i++) {
+        //     ans += Math.min(leftMax[i], rightMax[i]) - height[i];
+        // }
+
+        // return ans;
+
         
     }
 }
