2ba612f27cec1cd5cf89c69a6c55f0266fb07e6d
Bharath-chary02 <kadiyalabharath98@gmail.com>
2026-01-24 16:30:45 +0530

    Implement merge sort + two pointer counting for Reverse Pairs (O(n log n))

--- src/Array/ReversePairs493.java
+++ src/Array/ReversePairs493.java
@@ -20,7 +20,64 @@ public class ReversePairs493 {
 
 class Solution {
 
-    
+    public static void merge(int[] nums, int low, int mid, int high) {
+        int n1 = mid - low + 1;
+        int n2 = high - mid;
+        int[] left = new int[n1];
+        int[] right = new int[n2];
+
+        for(int i = 0; i < n1; i++) {
+            left[i] = nums[low + i];
+        }
+
+        for(int i = 0; i < n2; i++) {
+            right[i] = nums[mid + 1 + i];
+        }
+
+        int k = low, i = 0, j = 0;
+        while(i < n1 && j < n2) {
+            if(left[i] <= right[j]) {
+                nums[k++] = left[i++];
+            } else {
+                nums[k++] = right[j++];
+            }
+        }
+
+        while(i < n1) {
+            nums[k++] = left[i++];
+        }
+
+        while(j < n2) {
+            nums[k++] = right[j++];
+        }
+    }
+
+    public static int countPairs(int[] nums, int low, int mid, int high) {
+        int right = mid + 1;
+        int count = 0;
+
+        for(int i = low; i <= mid; i++) {
+            while(right <= high && (long) nums[i] > 2L * nums[right]) {
+                right++;
+            }
+            count += right - (mid + 1);
+        }
+
+        return count;
+    }
+
+    public static int mergeSort(int[] nums, int low, int high) {
+        int count = 0;
+        if(low < high) {
+            int mid = low + (high - low) / 2;
+            count += mergeSort(nums, low, mid);
+            count += mergeSort(nums, mid + 1, high);
+            count += countPairs(nums, low, mid, high);
+            merge(nums, low, mid, high);
+        }
+
+        return count;
+    }
 
     public int reversePairs(int[] nums) {
 
