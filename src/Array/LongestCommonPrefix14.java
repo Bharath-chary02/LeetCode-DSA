1f5d1c47fab5be4c0b8b75ac0733b92344d2c7ba
Bharath-chary02 <kadiyalabharath98@gmail.com>
2026-02-03 17:35:21 +0530

    Optimize longest common prefix using horizontal scanning

--- src/Array/LongestCommonPrefix14.java
+++ src/Array/LongestCommonPrefix14.java
@@ -41,6 +41,26 @@ class Solution {
 
         // return res;
 
-        
+        // ---------------------------------------------
+        // Approach 2: Horizontal Scanning (Optimal)
+        // Time: O(n * k)
+        // Space: O(1)
+
+        if(strs.length == 0) {
+            return "";
+        }
+
+        String prefix = strs[0];
+
+        for(int i = 1; i < strs.length; i++) {
+            while(!strs[i].startsWith(prefix)) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+                if(prefix.isEmpty()) {
+                    return "";
+                }
+            }
+        }
+
+        return prefix;
     }
 }
