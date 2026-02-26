1e788236cec80fb925d4180886c61d1892ff63c6
Bharath-chary02 <kadiyalabharath98@gmail.com>
2026-02-26 16:49:18 +0530

    Optimize middle node search using slow and fast pointer technique

--- src/MiddleoftheLinkedList876.java
+++ src/MiddleoftheLinkedList876.java
@@ -78,6 +78,19 @@ class Solution {
 
         // return temp;
 
-        
+        // ---------------------------------------------
+        // Approach 2: Optimal (Slow & Fast Pointer)
+        // Time: O(n)
+        // Space: O(1)
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
     }
 }
