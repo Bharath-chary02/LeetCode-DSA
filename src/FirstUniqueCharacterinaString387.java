20aa4af09805230399da815c3801d2c73f8ad3b9
Bharath-chary02 <kadiyalabharath98@gmail.com>
2026-03-11 21:27:21 +0530

    Add brute force solution using queue to find first unique character

---
+++ src/FirstUniqueCharacterinaString387.java
@@ -0,0 +1,52 @@
+// import java.util.*;
+
+public class FirstUniqueCharacterinaString387 {
+    public static void main(String[] args) {
+
+        Solution sol = new Solution();
+
+        String s1 = "leetcode";
+        String s2 = "loveleetcode";
+        String s3 = "aabb";
+
+        System.out.println(sol.firstUniqChar(s1)); // 0
+        System.out.println(sol.firstUniqChar(s2)); // 2
+        System.out.println(sol.firstUniqChar(s3)); // -1
+    }
+}
+
+class Solution {
+    public int firstUniqChar(String s) {
+
+        // ---------------------------------------------
+        // Approach 1: Brute Force (Queue Simulation)
+        // Time: O(n)
+        // Space: O(n)
+
+        // Queue<Character> q = new LinkedList<>();
+
+        // int[] freq = new int[26];
+
+        // for(int i = 0; i < s.length(); i++) {
+        //     char ch = s.charAt(i);
+        //     freq[ch - 'a']++;
+        // }
+
+        // for(int i = 0; i < s.length(); i++) {
+        //     q.add(s.charAt(i));
+        // }
+
+        // for(int i = 0; i < s.length(); i++) {
+        //     if(!q.isEmpty()) {
+        //         char ch = q.remove();
+        //         if(freq[ch - 'a'] == 1) {
+        //             return i;
+        //         }
+        //     }
+        // }
+
+        // return -1;
+
+        
+    }
+}
