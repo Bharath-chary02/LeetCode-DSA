613ed4ca58dd3624c316c06f16f0343475729548
Bharath-chary02 <kadiyalabharath98@gmail.com>
2026-03-13 17:06:26 +0530

    Optimize smaller numbers problem using counting array instead of sorting

--- src/HowManyNumbersAreSmallerThantheCurrentNumber1365.java
+++ src/HowManyNumbersAreSmallerThantheCurrentNumber1365.java
@@ -40,6 +40,27 @@ class Solution {
 
         // return res;
 
-        
+        // ---------------------------------------------
+        // Approach 2: Frequency + Prefix Sum (Optimal)
+        // Time: O(n)
+        // Space: O(1) because range is fixed (101)
+
+        int n = nums.length;
+        int[] freq = new int[101];
+        for(int i = 0; i < n; i++) {
+            freq[nums[i]]++;
+        }
+        for(int i = 1; i < 101; i++) {
+            freq[i] = freq[i - 1] + freq[i];
+        }
+        for(int i = 0; i < n; i++) {
+            if(nums[i] == 0) {
+                nums[i] = 0;
+                continue;
+            }
+            nums[i] = freq[nums[i] - 1];
+        }
+
+        return nums;
     }
 }
